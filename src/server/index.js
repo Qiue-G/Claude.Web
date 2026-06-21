c329539ae4915a4050762adf121e9fcb4b2e70cb
Qiue-G <1074842215@qq.com>
2026-06-21 17:05:07 +0800

    fix: improve ANSI stripping regex

--- src/server/index.js
+++ src/server/index.js
@@ -30,7 +30,7 @@ const WORKSPACE_DIR = process.env.WORKSPACE_DIR || join(__dirname, '../../worksp
 const MAX_SESSIONS = parseInt(process.env.MAX_SESSIONS || '10');
 const FREE_CODE_DIR = process.env.FREE_CODE_DIR || '/free-code';
 
-const VERSION = '4.0.4';
+const VERSION = '4.0.5';
 
 const sessions = new Map();
 const sessionProcesses = new Map();
