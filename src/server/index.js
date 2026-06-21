e4c25d12b3637a125dc4134c70eee02c90081dd9
Qiue-G <1074842215@qq.com>
2026-06-22 06:47:09 +0800

    bump v6.0.1 - trigger rebuild

--- src/server/index.js
+++ src/server/index.js
@@ -25,7 +25,7 @@ const HOST = process.env.HOST || '0.0.0.0';
 const WORKSPACE_DIR = process.env.WORKSPACE_DIR || join(__dirname, '../../workspace');
 const MAX_SESSIONS = parseInt(process.env.MAX_SESSIONS || '10');
 const FREE_CODE_DIR = process.env.FREE_CODE_DIR || '/free-code';
-const VERSION = '6.0.0';
+const VERSION = '6.0.1';
 
 const sessions = new Map();
 const sessionProcesses = new Map();
